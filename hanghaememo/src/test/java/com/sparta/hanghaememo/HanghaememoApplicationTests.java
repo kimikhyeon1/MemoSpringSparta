f277d9cd699337722f6d342e240f53229c676348
ikhyeon kim <101814294+kimikhyeon1@users.noreply.github.com>
2022-12-09 10:25:15 +0900

    Add files via upload

---
+++ hanghaememo/src/test/java/com/sparta/hanghaememo/HanghaememoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sparta.hanghaememo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+
+class HanghaememoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
